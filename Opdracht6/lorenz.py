7c0c0939a15c4062864f7d72834bb4052090b2c5
a4144805 <m.verheije@students.uu.nl>
2015-06-04 12:55:23 +0000

    Toevoeging van lorenz.py

---
+++ Opdracht6/lorenz.py
@@ -0,0 +1,26 @@
+from scipy.integrate import odeint
+from array import array
+
+
+
+class Lorenz(object):
+    """textje"""
+    def __init__(self,kaas,sigma,rho,beta):
+        self.sigma = sigma
+        self.rho = rho
+        self.beta = beta
+        self.elements = kaas
+    def f(self,X,t):
+        [x,y,z] = X
+        f = [self.sigma*(x - y), x*(self.rho - z) - y, x*y - (self.beta)*z]
+        return f
+    def solve(self, T, dt):
+        v = []
+        m = 0
+        while m <= T:
+            v.append(m)
+            m = m + dt
+        u = odeint(self.f,self.elements,v)
+        return u
+    def __str__(self):
+        return str(self.elements)
